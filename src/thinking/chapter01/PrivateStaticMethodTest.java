027781713d8b840738d8552bdf75474c03241a1d
wangzhengyu <121260020@qq.com>
2017-11-16 14:56:42 +0800

    private static

---
+++ src/thinking/chapter01/PrivateStaticMethodTest.java
@@ -0,0 +1,39 @@
+package thinking.chapter01;
+/**
+ * 静态私有方法测试
+ * @author Mr.w
+ *
+ */
+public class PrivateStaticMethodTest {
+	public static void main(String[] args) {
+//		WithStaticMethod.a();
+		WithStaticMethod.b();
+//		int n = WithStaticMethod.i;
+		int m = WithStaticMethod.j;
+		
+//		new WithStaticMethod().a();
+		new WithStaticMethod().b(); 
+	}
+}
+
+class WithStaticMethod{
+	private static int i=1;
+	public static int j=2;
+	
+	private static void a(){
+		System.out.println("a");
+		System.out.println(i);
+	}
+	
+	public static void b(){
+		System.out.println("b");
+		System.out.println(j);
+	}
+	
+	public static void main(String[] args) {
+		a();
+		b();
+		WithStaticMethod.a();
+		WithStaticMethod.b();
+	}
+}
