6544d31a9e2b8a15b1a43230668f13349d79dfe8
wangzhengyu <121260020@qq.com>
2017-11-16 17:21:22 +0800

    控制执行流程

---
+++ src/thinking/chapter04/ForeachTest.java
@@ -0,0 +1,16 @@
+package thinking.chapter04;
+
+/**
+ * 增强for循环测试
+ * @author Mr.w
+ *
+ */
+public class ForeachTest {
+	static int[] i = {1,2,3,4,5};
+	
+	public static void main(String[] args) {
+		for (Integer j : i) {
+			System.out.println(j);
+		}
+	}
+}
